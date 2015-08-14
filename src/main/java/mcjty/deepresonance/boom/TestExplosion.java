bafdf9402c6c982e8eaf494aee162eb321e57a06
Elec332 <arnout1998@gmail.com>
2015-08-14 10:51:20 +0200

    Fix explosions?

--- src/main/java/mcjty/deepresonance/boom/TestExplosion.java
+++ src/main/java/mcjty/deepresonance/boom/TestExplosion.java
@@ -2,8 +2,11 @@ package mcjty.deepresonance.boom;
 
 import elec332.core.explosion.Elexplosion;
 import elec332.core.util.BlockLoc;
+import elec332.core.world.WorldHelper;
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
@@ -26,5 +29,25 @@ public class TestExplosion extends Elexplosion{
     @Override
     protected void preExplode() {
         damageEntities(getRadius(), 4.0f);
+    }
+
+    protected void doExplode() {
+        if(!this.getWorld().isRemote) {
+            for(int x = (int)(-this.getRadius()); (float)x < this.getRadius(); ++x) {
+                for(int y = (int)(-this.getRadius()); (float)y < this.getRadius(); ++y) {
+                    for(int z = (int)(-this.getRadius()); (float)z < this.getRadius(); ++z) {
+                        BlockLoc targetPosition = this.getLocation().copy().translate(new BlockLoc(x, y, z));
+                        double dist = this.getLocation().distance(targetPosition);
+                        if(dist < (double)this.getRadius()) {
+                            Block block = WorldHelper.getBlockAt(this.getWorld(), targetPosition);
+                            if(block != null && !block.isAir(this.getWorld(), targetPosition.xCoord, targetPosition.yCoord, targetPosition.zCoord) && block != Blocks.bedrock && (dist < (double)(this.getRadius() - 1.0F) || (double)this.getWorld().rand.nextFloat() > 0.7D)) {
+                                block.onBlockExploded(getWorld(), targetPosition.xCoord, targetPosition.yCoord, targetPosition.zCoord, this);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
