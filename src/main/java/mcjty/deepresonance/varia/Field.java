c348c18fd2ccf4c8a8f3cd572d92af245fb67acc
McJty <mcjty1@gmail.com>
2015-08-23 19:30:46 +0200

    Work on radiation field. WIP

--- src/main/java/mcjty/deepresonance/varia/Field.java
+++ src/main/java/mcjty/deepresonance/varia/Field.java
@@ -16,16 +16,16 @@ public class Field {
         d = 1.0f / radius;
 
         // Initialize a 'border'
-        for (int i = 0 ; i < dim+2 ; i++) {
-            for (int j = 0 ; j < dim+2 ; j++) {
-                map[i][j][0] = Float.MAX_VALUE;
-                map[i][j][dim+1] = Float.MAX_VALUE;
-                map[i][0][j] = Float.MAX_VALUE;
-                map[i][dim+1][j] = Float.MAX_VALUE;
-                map[0][i][j] = Float.MAX_VALUE;
-                map[dim+1][i][j] = Float.MAX_VALUE;
-            }
-        }
+//        for (int i = 0 ; i < dim+2 ; i++) {
+//            for (int j = 0 ; j < dim+2 ; j++) {
+//                map[i][j][0] = Float.MAX_VALUE;
+//                map[i][j][dim+1] = Float.MAX_VALUE;
+//                map[i][0][j] = Float.MAX_VALUE;
+//                map[i][dim+1][j] = Float.MAX_VALUE;
+//                map[0][i][j] = Float.MAX_VALUE;
+//                map[dim+1][i][j] = Float.MAX_VALUE;
+//            }
+//        }
     }
 
     public float get(int x, int y, int z) {
@@ -63,15 +63,72 @@ public class Field {
         float f121 = set(x, y+1, z, value-d);
         float f110 = set(x, y, z-1, value-d);
         float f112 = set(x, y, z+1, value-d);
-        float f010 = set(x-1, y, z-1, Math.max(f011 * .7f, f110 * .7f));
-        float f210 = set(x+1, y, z-1, Math.max(f211 * .7f, f110 * .7f));
-        float f012 = set(x+1, y, z-1, Math.max(f011 * .7f, f112 * .7f));
-        float f212 = set(x+1, y, z-1, Math.max(f211 * .7f, f112 * .7f));
+
+        float f010 = set(x-1, y, z-1, Math.max(f011 - .7f * d, f110 - .7f * d));
+        float f210 = set(x+1, y, z-1, Math.max(f211 - .7f * d, f110 - .7f * d));
+        float f012 = set(x+1, y, z-1, Math.max(f011 - .7f * d, f112 - .7f * d));
+        float f212 = set(x+1, y, z-1, Math.max(f211 - .7f * d, f112 - .7f * d));
+
+        float f102 = set(x, y-1, z+1, Math.max(f101 - .7f * d, f112 - .7f * d));
+        float f001 = set(x-1, y-1, z, Math.max(f101 - .7f * d, f011 - .7f * d));
+        float f201 = set(x+1, y-1, z, Math.max(f101 - .7f * d, f211 - .7f * d));
+        float f100 = set(x, y-1, z-1, Math.max(f101 - .7f * d, f110 - .7f * d));
+
+        float f122 = set(x, y+1, z+1, Math.max(f121 - .7f * d, f112 - .7f * d));
+        float f021 = set(x-1, y+1, z, Math.max(f121 - .7f * d, f011 - .7f * d));
+        float f221 = set(x+1, y+1, z, Math.max(f121 - .7f * d, f211 - .7f * d));
+        float f120 = set(x, y+1, z-1, Math.max(f121 - .7f * d, f110 - .7f * d));
+
+        float f002 = set(x-1, y-1, z+1, Math.max(Math.max(f001 - .5f * d, f102 - .5f * d), f012 - .5f * d));
+        float f202 = set(x+1, y-1, z+1, Math.max(Math.max(f201 - .5f * d, f102 - .5f * d), f212 - .5f * d));
+        float f000 = set(x-1, y-1, z-1, Math.max(Math.max(f001 - .5f * d, f100 - .5f * d), f010 - .5f * d));
+        float f200 = set(x+1, y-1, z-1, Math.max(Math.max(f201 - .5f * d, f100 - .5f * d), f210 - .5f * d));
+
+        float f022 = set(x-1, y+1, z+1, Math.max(Math.max(f021 - .5f * d, f122 - .5f * d), f012 - .5f * d));
+        float f222 = set(x+1, y+1, z+1, Math.max(Math.max(f221 - .5f * d, f122 - .5f * d), f212 - .5f * d));
+        float f020 = set(x-1, y+1, z-1, Math.max(Math.max(f021 - .5f * d, f120 - .5f * d), f010 - .5f * d));
+        float f220 = set(x+1, y+1, z-1, Math.max(Math.max(f221 - .5f * d, f120 - .5f * d), f210 - .5f * d));
+
         return value;
     }
 
     public static interface Blocker {
         // A value between 0 and 1 indicating how much of the radiation is blocked. 0 means totally blocked. 1 is totally not blocked
         float getBlockingFactor(int x, int y, int z);
+    }
+
+    public static char d(float v) {
+        if (v >= 0.99f) {
+            return '#';
+        } else if (v >= 0.80f) {
+            return '*';
+        } else if (v >= 0.60f) {
+            return '+';
+        } else if (v >= 0.40f) {
+            return '-';
+        } else if (v >= 0.20f) {
+            return '.';
+        } else {
+            return ' ';
+        }
+    }
+
+    public static void main(String[] args) {
+        Field field = new Field(3, new Blocker() {
+            @Override
+            public float getBlockingFactor(int x, int y, int z) {
+                return 1;
+            }
+        });
+        field.set(3, 3, 3, 1.0f);
+        for (int y = 0 ; y < 3+3+1 ; y++) {
+            System.out.println("======================================================: y=" + y);
+            for (int z = 0 ; z < 3+3+1 ; z++) {
+                for (int x = 0 ; x < 3+3+1 ; x++) {
+                    System.out.print(d(field.get(x, y, z)));
+                }
+                System.out.println("");
+            }
+        }
     }
 }
