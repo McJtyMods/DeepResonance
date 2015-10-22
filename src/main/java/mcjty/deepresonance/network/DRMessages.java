7e85af6fc8068d5543c641a38480d49a29f2fc14
Elec332 <arnout1998@gmail.com>
2015-10-22 21:30:59 +0200

    Fix DR packets

--- src/main/java/mcjty/deepresonance/network/DRMessages.java
+++ src/main/java/mcjty/deepresonance/network/DRMessages.java
@@ -1,21 +1,15 @@
 package mcjty.deepresonance.network;
 
-import com.google.common.collect.Lists;
-import com.google.common.reflect.ClassPath;
 import cpw.mods.fml.relauncher.Side;
-import elec332.core.network.NetworkHandler;
 import mcjty.deepresonance.DeepResonance;
-
-import java.util.List;
+import mcjty.lib.network.PacketHandler;
 
 public class DRMessages {
 
-    private static List<Class> mcJtyLibPackets = Lists.newArrayList();
-
     public static void registerNetworkMessages() {
-        discoverPackets();
-        for (Class clazz : mcJtyLibPackets)
-            DeepResonance.networkHandler.registerPacket(clazz, getSideForPacket(clazz));
+
+        int i = PacketHandler.registerMessages(DeepResonance.networkHandler.getNetworkWrapper());
+        DeepResonance.networkHandler.setMessageIndex(i);
 
         // Server side
         DeepResonance.networkHandler.registerPacket(PacketGetGeneratorInfo.class, Side.SERVER);
@@ -26,25 +20,6 @@ public class DRMessages {
         DeepResonance.networkHandler.registerPacket(PacketReturnGeneratorInfoHandler.class,PacketReturnGeneratorInfo.class, Side.CLIENT);
         DeepResonance.networkHandler.registerPacket(PacketReturnCrystalInfoHandler.class, PacketReturnCrystalInfo.class, Side.CLIENT);
         DeepResonance.networkHandler.registerPacket(PacketReturnRadiationHandler.class, PacketReturnRadiation.class, Side.CLIENT);
-    }
-
-    private static void discoverPackets(){
-        try {
-            for (ClassPath.ClassInfo classInfo : ClassPath.from(ClassLoader.getSystemClassLoader()).getTopLevelClasses("mcjty.lib.network")) {
-                Class clazz = Class.forName(classInfo.getName());
-                if (NetworkHandler.isValidPacket(clazz))
-                    mcJtyLibPackets.add(clazz);
-            }
-        } catch (Exception e){
-            throw new RuntimeException("[DeepResonance] Error fetching packets!", e);
-        }
-    }
-
-    private static Side getSideForPacket(Class packetClass){
-        String name = packetClass.getName();
-        if (name.contains("From") && name.contains("Server") && !name.contains("Request"))
-            return Side.CLIENT;
-        return Side.SERVER;
     }
 
 }
