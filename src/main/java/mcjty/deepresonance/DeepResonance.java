00fbdb01ce6acf38bc2d38cf3be4c96739709a12
McJty <mcjty1@gmail.com>
2015-11-22 07:59:56 +0100

    Version 1.1.1

--- src/main/java/mcjty/deepresonance/DeepResonance.java
+++ src/main/java/mcjty/deepresonance/DeepResonance.java
@@ -36,7 +36,7 @@ import java.io.File;
         version = DeepResonance.VERSION)
 public class DeepResonance implements ModBase {
     public static final String MODID = "deepresonance";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
     public static final String MIN_FORGE_VER = "10.13.2.1291";
     public static final String MIN_MCJTYLIB_VER = "1.7.0";
     public static final String MIN_ELECCORE_VER = "1.4.176";
