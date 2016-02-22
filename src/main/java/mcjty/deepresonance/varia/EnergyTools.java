a5e125935ae0d263f357fbe82d36e5582ca2c01f
McJty <mcjty1@gmail.com>
2016-02-22 12:38:32 +0100

    Upgraded forge and mcjtylib

--- src/main/java/mcjty/deepresonance/varia/EnergyTools.java
+++ src/main/java/mcjty/deepresonance/varia/EnergyTools.java
@@ -9,13 +9,11 @@ import net.minecraft.util.EnumFacing;
 public class EnergyTools {
 
     public static boolean isEnergyTE(TileEntity te) {
-        return te instanceof IEnergyHandler || te instanceof IEnergyReceiver || te instanceof IEnergyProvider;
+        return te instanceof IEnergyHandler;
     }
 
     public static int extractEnergy(TileEntity tileEntity, EnumFacing from, int maxExtract) {
-        if (tileEntity instanceof IEnergyHandler) {
-            return ((IEnergyHandler) tileEntity).extractEnergy(from, maxExtract, false);
-        } else if (tileEntity instanceof IEnergyProvider) {
+        if (tileEntity instanceof IEnergyProvider) {
             return ((IEnergyProvider) tileEntity).extractEnergy(from, maxExtract, false);
         } else {
             return 0;
@@ -23,9 +21,7 @@ public class EnergyTools {
     }
 
     public static int receiveEnergy(TileEntity tileEntity, EnumFacing from, int maxReceive) {
-        if (tileEntity instanceof IEnergyHandler) {
-            return ((IEnergyHandler) tileEntity).receiveEnergy(from, maxReceive, false);
-        } else if (tileEntity instanceof IEnergyReceiver) {
+        if (tileEntity instanceof IEnergyReceiver) {
             return ((IEnergyReceiver) tileEntity).receiveEnergy(from, maxReceive, false);
         } else {
             return 0;
