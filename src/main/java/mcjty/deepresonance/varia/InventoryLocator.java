a10bdf010b3a205c2cfbb5e71a6be35674c922c3
McJty <mcjty1@gmail.com>
2016-02-24 12:59:14 +0100

    Added support for IItemHandler insertion

--- src/main/java/mcjty/deepresonance/varia/InventoryLocator.java
+++ src/main/java/mcjty/deepresonance/varia/InventoryLocator.java
@@ -9,11 +9,40 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 public class InventoryLocator {
 
     private BlockPos inventoryCoordinate = null;
     private EnumFacing inventorySide = null;
+
+    public IItemHandler getItemHandlerAtDirection(World worldObj, BlockPos thisCoordinate, EnumFacing direction) {
+        if (direction == null) {
+            if (inventoryCoordinate != null) {
+                return getItemHandlerAtCoordinate(worldObj, inventoryCoordinate, inventorySide);
+            }
+            return null;
+        }
+        TileEntity te = WorldHelper.getTileAt(worldObj, thisCoordinate);
+        if (te != null && te.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, direction.getOpposite())) {
+            // Remember in inventoryCoordinate (acts as a cache)
+            inventoryCoordinate = thisCoordinate.offset(direction);
+            inventorySide = direction.getOpposite();
+            return getItemHandlerAtCoordinate(worldObj, inventoryCoordinate, inventorySide);
+        }
+        return null;
+    }
+
+    private IItemHandler getItemHandlerAtCoordinate(World worldObj, BlockPos c, EnumFacing direction) {
+        TileEntity te = WorldHelper.getTileAt(worldObj, c);
+        if (te != null && te.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, direction)) {
+            return te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, direction);
+        }
+        return null;
+    }
+
 
     public IInventory getInventoryAtDirection(World worldObj, BlockPos thisCoordinate, EnumFacing direction) {
         if (direction == null) {
@@ -37,18 +66,28 @@ public class InventoryLocator {
     }
 
     public void ejectStack(World worldObj, int x, int y, int z, ItemStack spentMaterial, BlockPos thisCoordinate, EnumFacing[] directions) {
-        boolean spawnInWorld = true;
         for (EnumFacing dir : directions) {
-            IInventory inventory = getInventoryAtDirection(worldObj, thisCoordinate, dir);
-            if (inventory != null) {
-                if (InventoryHelper.mergeItemStackSafe(inventory, false, getInventorySide(), spentMaterial, 0, inventory.getSizeInventory(), null) == 0) {
-                    spawnInWorld = false;
-                    break;
+            IItemHandler handler = getItemHandlerAtDirection(worldObj, thisCoordinate, dir);
+            if (spentMaterial == null) {
+                break;
+            }
+
+            if (handler != null) {
+                spentMaterial = ItemHandlerHelper.insertItem(handler, spentMaterial, false);
+            } else {
+                IInventory inventory = getInventoryAtDirection(worldObj, thisCoordinate, dir);
+                if (inventory != null) {
+                    int amount = InventoryHelper.mergeItemStackSafe(inventory, false, getInventorySide(), spentMaterial, 0, inventory.getSizeInventory(), null);
+                    if (amount == 0) {
+                        spentMaterial = null;
+                    } else {
+                        spentMaterial.stackSize = amount;
+                    }
                 }
             }
         }
 
-        if (spawnInWorld) {
+        if (spentMaterial != null) {
             EntityItem entityItem = new EntityItem(worldObj, x, y, z, spentMaterial);
             worldObj.spawnEntityInWorld(entityItem);
         }
