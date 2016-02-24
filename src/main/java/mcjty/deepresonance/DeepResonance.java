3c648b4d6b2810e5ea4c86bf472ee41307fd74bf
Elec332 <arnout1998@gmail.com>
2016-02-24 21:43:16 +0100

    Update ElecCore

--- src/main/java/mcjty/deepresonance/DeepResonance.java
+++ src/main/java/mcjty/deepresonance/DeepResonance.java
@@ -39,7 +39,7 @@ public class DeepResonance implements ModBase {
     public static final String VERSION = "1.1.4";
     public static final String MIN_FORGE_VER = "10.13.2.1291";
     public static final String MIN_MCJTYLIB_VER = "1.8.1";
-    public static final String MIN_ELECCORE_VER = "1.4.176";
+    public static final String MIN_ELECCORE_VER = "1.4.231";
 
     @SidedProxy(clientSide="mcjty.deepresonance.proxy.ClientProxy", serverSide="mcjty.deepresonance.proxy.ServerProxy")
     public static CommonProxy proxy;
