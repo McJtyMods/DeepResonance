0e6e8b21b9fff5ae6a5cb328e930cdba2f2d42be
McJty <mcjty1@gmail.com>
2016-04-07 14:49:10 +0200

    1.1.4beta27

--- src/main/java/mcjty/deepresonance/DeepResonance.java
+++ src/main/java/mcjty/deepresonance/DeepResonance.java
@@ -39,9 +39,9 @@ import java.io.File;
         version = DeepResonance.VERSION)
 public class DeepResonance implements ModBase {
     public static final String MODID = "deepresonance";
-    public static final String VERSION = "1.1.4beta26";
+    public static final String VERSION = "1.1.4beta27";
     public static final String MIN_FORGE_VER = "12.16.0.1835";
-    public static final String MIN_MCJTYLIB_VER = "1.8.9-1.8.1beta13";
+    public static final String MIN_MCJTYLIB_VER = "1.8.9-1.8.1beta14";
     public static final String MIN_ELECCORE_VER = "1.4.259";
 
     @SidedProxy(clientSide="mcjty.deepresonance.proxy.ClientProxy", serverSide="mcjty.deepresonance.proxy.ServerProxy")
