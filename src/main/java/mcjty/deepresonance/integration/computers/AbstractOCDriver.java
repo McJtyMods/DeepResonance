748fb617d0cb1b7857e57faab89ce942fc3eb25d
Martin Zeller <mz.bremerhaven@gmail.com>
2016-07-22 22:29:07 +0200

    Implement OC Integration framework

---
+++ src/main/java/mcjty/deepresonance/integration/computers/AbstractOCDriver.java
@@ -0,0 +1,63 @@
+package mcjty.deepresonance.integration.computers;
+
+import li.cil.oc.api.Network;
+import li.cil.oc.api.driver.NamedBlock;
+import li.cil.oc.api.network.Visibility;
+import li.cil.oc.api.prefab.DriverSidedTileEntity;
+import li.cil.oc.api.prefab.ManagedEnvironment;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public abstract class AbstractOCDriver extends DriverSidedTileEntity {
+    String componentName;
+    Class<? extends TileEntity> clazz;
+
+    public AbstractOCDriver(String componentName, Class<? extends TileEntity> clazz) {
+        this.componentName = componentName;
+        this.clazz = clazz;
+    }
+
+    public abstract static class InternalManagedEnvironment<T> extends ManagedEnvironment implements NamedBlock {
+        protected T tile;
+        private String componentName;
+
+        public InternalManagedEnvironment(T tile, String name) {
+            this.tile = tile;
+            this.componentName = name;
+            this.setNode(Network.newNode(this, Visibility.Network).withComponent(componentName, Visibility.Network).create());
+        }
+
+        @Override
+        public String preferredName() {
+            return componentName;
+        }
+
+        @Override
+        public int priority() {
+            return 0;
+        }
+    }
+
+    @Override
+    public Class<?> getTileEntityClass() {
+        return clazz;
+    }
+
+    @Override
+    public boolean worksWith(World world, BlockPos pos, EnumFacing side) {
+        return clazz.isInstance(world.getTileEntity(pos));
+    }
+
+    @Override
+    public ManagedEnvironment createEnvironment(World world, BlockPos pos, EnumFacing side) {
+        TileEntity tile = world.getTileEntity(pos);
+        if (tile.getClass().isInstance(tile)) {
+            return this.createEnvironment(world, pos, side, tile);
+        }
+        return null;
+    }
+
+    public abstract ManagedEnvironment createEnvironment(World world, BlockPos pos, EnumFacing side, TileEntity tile);
+}
