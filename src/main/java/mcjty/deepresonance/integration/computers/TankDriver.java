aa674de2d3e4e7bc3dcd7e85868e5b7e6dc24e62
Martin Zeller <mz.bremerhaven@gmail.com>
2016-07-22 22:29:49 +0200

    Add OC Driver for tanks

---
+++ src/main/java/mcjty/deepresonance/integration/computers/TankDriver.java
@@ -0,0 +1,74 @@
+package mcjty.deepresonance.integration.computers;
+
+import li.cil.oc.api.driver.NamedBlock;
+import li.cil.oc.api.machine.Arguments;
+import li.cil.oc.api.machine.Callback;
+import li.cil.oc.api.machine.Context;
+import li.cil.oc.api.prefab.DriverSidedTileEntity;
+import li.cil.oc.api.prefab.ManagedEnvironment;
+import li.cil.oc.api.Network;
+import li.cil.oc.api.network.Visibility;
+import mcjty.deepresonance.blocks.tank.TileTank;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class TankDriver {
+    public static class OCDriver extends AbstractOCDriver {
+        public OCDriver() {
+            super("deepresonance_tank", TileTank.class);
+        }
+
+        public static class InternalManagedEnvironment extends AbstractOCDriver.InternalManagedEnvironment<TileTank> {
+            public InternalManagedEnvironment(TileTank tile) {
+                super(tile, "deepresonance_tank");
+            }
+
+			@Callback(doc="function():number; Returns the maximum amount of fluid")
+			public Object[] getCapacity(Context c, Arguments a) {
+			    return new Object[]{tile.getCapacity()};
+            }
+
+            @Callback(doc="function():number; Returns the current amount of fluid")
+            public Object[] getFluidAmount(Context c, Arguments a) {
+                return new Object[]{tile.getFluidAmount()};
+            }
+
+            @Callback(doc="function():number; Returns the quality of the RCL")
+            public Object[] getQuality(Context c, Arguments a) {
+                NBTTagCompound tagData = tile.getFluidTag();
+                return new Object[]{tagData.getFloat("quality")};
+            }
+
+            @Callback(doc="function():number; Returns the purity of the RCL")
+            public Object[] getPurity(Context c, Arguments a) {
+                NBTTagCompound tagData = tile.getFluidTag();
+                return new Object[]{tagData.getFloat("purity")};
+            }
+
+            @Callback(doc="function():number; Returns the strength of the RCL")
+            public Object[] getStrength(Context c, Arguments a) {
+                NBTTagCompound tagData = tile.getFluidTag();
+                return new Object[]{tagData.getFloat("strength")};
+            }
+
+            @Callback(doc="function():number; Returns the efficiency of the RCL")
+            public Object[] getEfficiency(Context c, Arguments a) {
+                NBTTagCompound tagData = tile.getFluidTag();
+                return new Object[]{tagData.getFloat("efficiency")};
+            }
+
+            @Override
+            public int priority() {
+                return 4;
+            }
+        }
+
+        @Override
+        public ManagedEnvironment createEnvironment(World world, BlockPos pos, EnumFacing side, TileEntity tile) {
+            return new InternalManagedEnvironment((TileTank)tile);
+        }
+    }
+}
