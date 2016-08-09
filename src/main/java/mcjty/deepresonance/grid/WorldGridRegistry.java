105c7d3ec6b479b6a0b0f0612b824b6ae874c049
Elec332 <arnout1998@gmail.com>
2016-08-09 12:41:00 +0200

    Removed duct-system, we don't use it

--- src/main/java/mcjty/deepresonance/grid/WorldGridRegistry.java
+++ src/main/java/mcjty/deepresonance/grid/WorldGridRegistry.java
@@ -2,11 +2,8 @@ package mcjty.deepresonance.grid;
 
 import elec332.core.multiblock.dynamic.AbstractDynamicMultiBlockWorldHolder;
 import elec332.core.registry.AbstractWorldRegistryHolder;
-import mcjty.deepresonance.grid.fluid.DRFluidWorldGridHolder;
-import mcjty.deepresonance.grid.fluid.EventHandler;
 import mcjty.deepresonance.grid.tank.DRTankWorldHolder;
 import net.minecraft.world.World;
-import net.minecraftforge.common.MinecraftForge;
 
 /**
  * Created by Elec332 on 3-8-2015.
@@ -14,17 +11,10 @@ import net.minecraftforge.common.MinecraftForge;
 public class WorldGridRegistry {
 
     public WorldGridRegistry(){
-        this.fluidGridWorldRegistry = new DRFluidGridWorldRegistry();
         this.tankGridWorldRegistry = new DRTankGridWorldRegistry();
-        MinecraftForge.EVENT_BUS.register(new EventHandler());
     }
 
-    private DRFluidGridWorldRegistry fluidGridWorldRegistry;
     private DRTankGridWorldRegistry tankGridWorldRegistry;
-
-    public DRFluidGridWorldRegistry getFluidRegistry() {
-        return fluidGridWorldRegistry;
-    }
 
     public DRTankGridWorldRegistry getTankRegistry() {
         return tankGridWorldRegistry;
@@ -43,22 +33,6 @@ public class WorldGridRegistry {
         @Override
         public AbstractDynamicMultiBlockWorldHolder newRegistry(World world) {
             return new DRTankWorldHolder(world);
-        }
-    }
-
-    public static class DRFluidGridWorldRegistry extends AbstractWorldRegistryHolder<DRFluidWorldGridHolder>{
-
-        private DRFluidGridWorldRegistry(){
-        }
-
-        @Override
-        public boolean serverOnly() {
-            return true;
-        }
-
-        @Override
-        public DRFluidWorldGridHolder newRegistry(World world) {
-            return new DRFluidWorldGridHolder(world);
         }
     }
 
