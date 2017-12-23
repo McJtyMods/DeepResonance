5afd64effb327a5168ca9ae1d9dd7fc9e11ec63d
McJty <mcjty1@gmail.com>
2017-12-23 06:41:24 +0100

    Version bump

--- src/main/java/mcjty/deepresonance/DeepResonance.java
+++ src/main/java/mcjty/deepresonance/DeepResonance.java
@@ -47,7 +47,7 @@ public class DeepResonance implements ModBase {
     public static final String MIN_ELECCORE_VER = "1.8.434";
     public static final String MIN_OPENCOMPUTERS_VER = "1.6.0";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
-    public static final String MIN_MCJTYLIB_VER = "2.5.2";
+    public static final String MIN_MCJTYLIB_VER = "2.5.3";
 
     @SidedProxy(clientSide="mcjty.deepresonance.proxy.ClientProxy", serverSide="mcjty.deepresonance.proxy.ServerProxy")
     public static CommonProxy proxy;
