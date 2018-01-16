bc70bd3ba99980d4d02d6c39241e987270b18132
Andrew W <marwat208@gmail.com>
2018-01-15 22:33:41 -0500

    Expanded IRadiationArmor
    Added default implementation of protection
    Added an ItemStack parameter to protection. This allows ItemStack/ NBT
    Sensitive implementation of the method.
    
    Added default method, isActive that a parameter of ItemStack.
    By default it returns true. Mods are able to override this to control if
    a given itemstack should function or not.
    
    Implemented IRadiationArmor changes to ItemRadiationSuit

--- src/main/java/mcjty/deepresonance/api/IRadiationArmor.java
+++ src/main/java/mcjty/deepresonance/api/IRadiationArmor.java
@@ -1,10 +1,20 @@
 package mcjty.deepresonance.api;
 
+import mcjty.deepresonance.radiation.RadiationConfiguration;
+import net.minecraft.item.ItemStack;
+
 /**
  * @author canitzp
  */
 public interface IRadiationArmor {
-
-    float[] protection();
-
+    
+    default float[] protection(ItemStack armorItemStack) {
+    
+        return RadiationConfiguration.suitProtection;
+    }
+    
+    default boolean isActive(ItemStack armorItemStack) {
+        
+        return true;
+    }
 }
