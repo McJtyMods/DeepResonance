be384f31f2906edc38dddae47b425172e91bf240
McJty <mcjty1@gmail.com>
2018-05-29 09:35:49 +0200

    Fixed versions in all mods

--- src/main/java/mcjty/deepresonance/DeepResonance.java
+++ src/main/java/mcjty/deepresonance/DeepResonance.java
@@ -34,11 +34,11 @@ import java.io.File;
         version = DeepResonance.VERSION)
 public class DeepResonance implements ModBase {
     public static final String MODID = "deepresonance";
-    public static final String VERSION = "1.7.0alpha";
+    public static final String VERSION = "1.7.0-alpha";
     public static final String MIN_ELECCORE_VER = "1.8.434";
     public static final String MIN_OPENCOMPUTERS_VER = "1.6.0";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
-    public static final String MIN_MCJTYLIB_VER = "3.0.0";
+    public static final String MIN_MCJTYLIB_VER = "3.0.0-alpha";
 
     @SidedProxy(clientSide="mcjty.deepresonance.proxy.ClientProxy", serverSide="mcjty.deepresonance.proxy.ServerProxy")
     public static CommonProxy proxy;
